53b3b28cb46b8a9704c14050a5e727687c59e562
Shailesh MS <shaileshms18@gmail.com>
2021-11-30 12:27:26 +0530

    first project

---
+++ Demo/src/com/ltts/DemoIf.java
@@ -0,0 +1,12 @@
+package com.ltts;
+
+public class DemoIf {
+	public static void main(String[] args){
+	       int  age = 20;
+	       if(age>=18)
+	       {
+	        System.out.print("elegible for vote ");
+	       }
+	    }
+	}
+
